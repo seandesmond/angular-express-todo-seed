27326e8709e55d8ad398e126a9ef0975fd20d2ef
Sean Desmond <sean.desmond@gmail.com>
2013-12-13 14:02:36 -0500

    Fix for Heroku compatibility.

--- app/lib/server.js
+++ app/lib/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function (app, config) {
+    var port = process.env.PORT || config.port || 3000;
     console.log('[express train application listening on %s]', config.port);
-    return app.listen(config.port);
+    return app.listen(port);
 };
